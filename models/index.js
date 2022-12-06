ba1b3b5e628eeda91f724438e690a73ee96bcc90
Impp18 <tdfallara@gmail.com>
2022-12-05 21:17:54 -0500

    updated routes further

--- models/index.js
+++ models/index.js
@@ -5,16 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+ Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+  });
 // Categories have many Products
-
+ Category.hasMany(Product, {
+  foreighKey: 'product_id',
+  onDelete: 'CASCADE',
+ })
 // Products belongToMany Tags (through ProductTag)
-
+ Product.belongsToMany(Tag, {
+  through:{
+    model: ProductTag,
+    Unique: false
+  }
+ })
 // Tags belongToMany Products (through ProductTag)
+ Tag.belongsToManu(Product, {
+  through:{
+    model: ProductTag,
+    Unique: false
+  }
+ })
+module.exports = { Product, Category, Tag, ProductTag};
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const productData = await Product.findALL(req.params.id, {
+      include: [{ model: Product, through: ProductTag}]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(travelerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const travelerData = await Traveler.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag}]
+    });
+    if (!travelerData) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(travelerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
